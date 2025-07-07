e3e1fcfe1d7219abc644bb609f613b8453d6cbd9
Abror git config --global user.name <abrorboymatov2005@gmail.com>
2025-07-07 21:40:13 +0500

    kskmad

---
+++ app.js
@@ -0,0 +1,46 @@
+// function decloration
+// function getnames(a) {
+//     console.log("names");
+// }
+// getnames(2)
+
+
+// 2 function expression
+// let getnames;
+// getNames = function (a) {
+//     console.log("names");  
+// }
+// getNames(2)
+// console.log(typeof getNames)
+
+
+// let res = 90;
+// console.log(res.toFixed());
+
+
+// Arrow function
+// let res = () => {};
+// function salom() {
+//     let res = (par = "sizga")=>{
+//     return "Jasur " + par;
+// };
+// console.log(res("")+ "salom ber");
+// }
+// salom();
+
+
+// IIFE =>immediatelty invoked function 
+// (function () {
+//     console.log("hammaga salom");  
+// })();
+
+
+
+// new Date();
+// let date = new Date();
+// console.log(date);
+
+
+// simple dom
+// html elementlarini tanlash
+
